58d5f98873f1e0c128c20f976d4c038dc4eb92d1
supakorn <mail@supakorn.me>
2026-04-02 17:01:07 +0700

    move FZF_DEFAULT_OPTS from fish universal vars into conf.d

--- private_dot_config/fish/conf.d/02_plugins.fish
+++ private_dot_config/fish/conf.d/02_plugins.fish
@@ -4,5 +4,11 @@ starship init fish | source
 # ---- Zoxide ----
 zoxide init fish | source
 
-# ---- FZF (optional if needed) ----
+# ---- FZF ----
 set -gx FZF_DEFAULT_COMMAND 'fd --type f'
+set -gx FZF_DEFAULT_OPTS "\
+  --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+  --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+  --color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
+  --color=selected-bg:#ccd0da \
+  --color=border:#7287fd,label:#4c4f69"
