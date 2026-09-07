a7d4032a07510f5b85e6d2b08b34a78fbc34f56b
Supakorn Ieamgomol <78318848+supakornn@users.noreply.github.com>
2026-09-08 00:46:09 +0700

    fix lazygit

--- common/.config/fish/conf.d/01-aliases.fish
+++ common/.config/fish/conf.d/01-aliases.fish
@@ -2,7 +2,7 @@ alias ll='eza --color=always --long --git --icons=always'
 alias la='eza -la --color=always --long --git --icons=always'
 alias ls='eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions --tree --level=2'
 function lg --wraps lazygit --description 'Lazygit with the catpuccin theme'
-  lazygit --useconfig-file="$HOME/.config/lazygit/config.yaml,$HOME/.config/lazygit/catppuccin-macchiato.yaml" $argv
+  lazygit --use-config-file="$HOME/.config/lazygit/config.yaml,$HOME/.config/lazygit/catppuccin-macchiato.yml" $argv
 end
 
 abbr -a .. 'cd ..'
