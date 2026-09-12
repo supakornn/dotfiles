45fc17e6381a2d46c83521b777ffb04bb9e1adba
Supakorn Ieamgomol <78318848+supakornn@users.noreply.github.com>
2026-09-12 13:00:29 +0700

    Replace Neovim configuration

---
+++ common/.config/nvim/lua/plugins/cloak.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"laytan/cloak.nvim",
+		lazy = false,
+		config = function()
+			require("cloak").setup({
+				patterns = {
+					{
+						file_pattern = "**/*.vars*",
+						cloak_pattern = "=.+",
+					},
+					{
+						file_pattern = "**/*.env*",
+						cloak_pattern = "=.+",
+					},
+					{
+						file_pattern = "**/secrets.fish",
+						cloak_pattern = "^(%s*set%s+%-[%w]+%s+[%w_]+%s+).+",
+						replace = "%1",
+					},
+					{
+						file_pattern = "**/*.opencode.json",
+						cloak_pattern = '("apiKey":) .+',
+						replace = "%1 ",
+					},
+					{
+						file_pattern = "**/config.toml",
+						cloak_pattern = "(token =) .+",
+						replace = "%1 ",
+					},
+				},
+			})
+		end,
+	},
+}
