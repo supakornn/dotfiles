e8ed25b2b0e536e9beea2aa1b99149578cc4f5d2
Supakorn Ieamgomol <78318848+supakornn@users.noreply.github.com>
2026-09-12 18:54:24 +0700

    Apply selected palette in Fish

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
-    fish_config theme choose catppuccin-macchiato
+    set -l color_theme dark
+    if test (command cat ~/.config/dotfiles/theme 2>/dev/null) = latte
+        set color_theme light
+    end
+    fish_config theme choose catppuccin-macchiato --color-theme=$color_theme
 end
