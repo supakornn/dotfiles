cf129d5105660d671b9f28c359b8893fcb90ac33
Supakorn Ieamgomol <78318848+supakornn@users.noreply.github.com>
2026-09-12 23:18:40 +0700

    fish

--- common/.config/fish/conf.d/01-aliases.fish
+++ common/.config/fish/conf.d/01-aliases.fish
@@ -1,6 +1,10 @@
 alias ll='eza --color=always --long --git --icons=always'
 alias la='eza -la --color=always --long --git --icons=always'
 alias ls='eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions --tree --level=2'
+alias oc="opencode"
+alias c="clear"
+
+
 function lg --wraps lazygit --description 'Lazygit with the Catppuccin theme'
   set -l flavor (command cat ~/.config/dotfiles/theme 2>/dev/null)
   test "$flavor" = latte; or set flavor macchiato
@@ -10,3 +14,4 @@ end
 abbr -a .. 'cd ..'
 abbr -a ... 'cd ../..'
 abbr -a reload 'source ~/.config/fish/config.fish'
+
