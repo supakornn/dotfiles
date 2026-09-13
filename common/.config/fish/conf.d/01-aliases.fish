633767593d0741de135b038b733b31afd16ae1b4
Supakorn Ieamgomol <78318848+supakornn@users.noreply.github.com>
2026-09-13 17:10:38 +0700

    Use shared LazyGit config for lg

--- common/.config/fish/conf.d/01-aliases.fish
+++ common/.config/fish/conf.d/01-aliases.fish
@@ -3,7 +3,7 @@ alias la='eza -la --color=always --long --git --icons=always'
 alias ls='eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions --tree --level=2'
 alias oc="opencode"
 alias c="clear"
-alias lg="lazygit"
+alias lg='lazygit --use-config-file="$HOME/.config/lazygit/config.yml"'
 
 abbr -a .. 'cd ..'
 abbr -a ... 'cd ../..'
