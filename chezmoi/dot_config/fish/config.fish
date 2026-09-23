2f96cca0adde619cd2503d824a55f42c66dec3f7
Supakorn Ieamgomol <78318848+supakornn@users.noreply.github.com>
2026-09-23 15:57:44 +0700

    update

--- chezmoi/dot_config/fish/config.fish
+++ chezmoi/dot_config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     fish_config theme choose catppuccin-macchiato --color-theme=dark
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
